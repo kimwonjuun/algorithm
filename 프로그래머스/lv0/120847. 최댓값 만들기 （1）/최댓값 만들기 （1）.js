051783eb2a29868cb019220180bf4fa6457b983e
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-03-23 01:17:42 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    let sortedNumbers = numbers.sort((a,b)=>b-a)
-    return sortedNumbers[0] * sortedNumbers[1]
+    numbers.sort((a,b)=>b-a)
+    return numbers[0] * numbers[1]
 }
