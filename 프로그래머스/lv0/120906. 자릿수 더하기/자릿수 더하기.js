9920afd5ec49e10d9d5f6f3a8c5657a044312724
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-03-27 01:18:13 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,8 @@
 function solution(n) {
+    // String() 숫자열을 문자열로 변환 -> '12345'
+    // split() 문자열을 잘라 배열로 변환 -> ['1', '2', '3', '4', '5']
+    // map() 으로 뿌려 parseInt() 를 통해 각 문자열을 숫자로 변환 (이게 맞나 싶음?) -> [1, 2, 3, 4, 5]
+    // reduce() 로 배열 내 요소 총합을 구함 -> 15
+    // 이렇게 길게 풀 일인지 ?
     return String(n).split('').map(v => parseInt(v)).reduce((a, b) => a + b)
-
 }
