d80e066d81378312e74ff55ac0faba52abe5711b
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-03-27 01:04:41 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,4 @@
 function solution(n) {
-    array = String(n).split('').map(v => parseInt(v)).reduce((a, b) => a + b)
-    return array
-    // return array.reduce((a, b) => a + b)
+    return String(n).split('').map(v => parseInt(v)).reduce((a, b) => a + b)
+
 }
