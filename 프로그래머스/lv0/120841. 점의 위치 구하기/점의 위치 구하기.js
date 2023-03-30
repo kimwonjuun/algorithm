ee902b77331466617e46fcc91ce73f5a38bd8f0d
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-03-30 10:59:20 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,6 @@
+function solution(dot) {
+    return dot[0] > 0 && dot[1] > 0 ? 1 :
+    dot[0] < 0 && dot[1] > 0 ? 2 :
+    dot[0] < 0 && dot[1] < 0 ? 3 :
+    dot[0] > 0 && dot[1] < 0 ? 4 : null
+}
