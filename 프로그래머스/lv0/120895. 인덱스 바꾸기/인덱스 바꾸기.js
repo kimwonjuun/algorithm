5e9c6ed73db21a9c3f776202a0d867984a4c509b
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-04-21 10:50:30 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,8 +1,9 @@
 function solution(my_string, num1, num2) {
-    arr = my_string.split('');
+    arr = my_string.split("");
     [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
-    return arr.join('');
+    return arr.join("");
     
+    // 좀 더 복잡도가 적음.
     // const arr = my_string.split("");
     // const temp = arr[num1];
     // arr[num1] = arr[num2];
