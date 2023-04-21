3ecdc2cbe14b6c8f56edaf89f0526a9002ac2d6b
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-04-21 10:51:08 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,9 +1,11 @@
 function solution(box, n) {
-    // 일일이 작성
+    // 일일이 작성 (가장 빠름)
     // return Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
+    //
     // reduce 메서드 사용
-    // 시간 복잡도는 reduce 메서드를 사용하지 않는 것이 4개 중 3개 테스트에서 0.01ms 빠름
-    // return box.reduce((acc, cur) => acc * Math.floor(cur / n), 1);
-    // map 메서드 사용
-    return box.map(v => parseInt(v / n)).reduce((acc, cur) => acc * cur);
+    // 시간 복잡도는 reduce 메서드를 사용하면 4개 중 3개 테스트에서 0.01ms 느림
+    //
+    return box.reduce((acc, cur) => acc * Math.floor(cur / n), 1);
+    // map 메서드 사용 (가장 느림)
+    // return box.map(v => parseInt(v / n)).reduce((acc, cur) => acc * cur);
 }
