3aae8c6b918e494b6c13ba44da2dbccc4a7525ad
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-04-22 02:16:27 +0900

    [level 0] Title: 합성수 찾기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,11 +1,11 @@
 function solution(n) {
-    let count = 0;
+    let answer = 0;
     for (let i = 1; i <= n; i++) {
-        let composite_num = 0;
+        let count = 0;
         for (let j = 1; j <= i; j++) {
-            if (i % j === 0) composite_num++;
+            if (i % j === 0) count++;
         }
-        if (composite_num >= 3) count++;
+        if (count >= 3) answer++;
     }
-    return count;
+    return answer;
 }
