be45133d737086bf1ce937744781a598b2442eaa
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-04-21 09:07:02 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,6 @@
 function solution(box, n) {
-    return Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
+    // 일일이 작성
+    // return Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
+    // reduce 메서드 사용
+    return box.reduce((acc, cur) => acc * Math.floor(cur / n), 1);
 }
