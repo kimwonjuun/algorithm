5919beec4e75fe183488eb756ca76c5d32b8f5fb
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-04-25 00:40:34 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -2,3 +2,14 @@ function solution(rsp) {
     let win_num = rsp.split("").map(v => v === "2" ? 0 : v === "0" ? 5 : 2);
     return win_num.join("");
 }
+
+// 다른 사람의 풀이
+// function solution(rsp) {
+//     let arr = {
+//         2: 0,
+//         0: 5,
+//         5: 2
+//     };
+//     let answer = [...rsp].map(v => arr[v]).join("");
+//     return answer;
+// }
