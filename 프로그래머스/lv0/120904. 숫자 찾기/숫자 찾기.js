50e412e793e4a67a486bedbc3a180aef05887472
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-05-01 23:57:47 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,4 @@
 function solution(num, k) {
     let str = num.toString();
-    return str.indexOf(k) !== -1 ? str.indexOf(k) + 1 : -1
+    return str.indexOf(k) !== -1 ? str.indexOf(k) + 1 : -1;
 }
