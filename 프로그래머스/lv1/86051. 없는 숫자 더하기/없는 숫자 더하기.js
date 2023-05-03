a588e4761115c4905d88ef5bb987d7b8aa978102
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-05-03 18:12:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
     let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(v => !numbers.includes(v));
-    return arr.reduce((a, b) => a + b);
+    return arr.reduce((acc, cur) => acc + cur, 0);
 }
