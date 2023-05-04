3c7171c952e08c398bda73a3fb88b0d5f37e5d9a
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-05-04 20:29:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,3 +1,8 @@
+// function solution(n) {
+//     return n % 2 === 0 ? "수박".repeat(n / 2) : "수박".repeat(n).slice(0, n);
+// }
+
+// 그냥 이렇게 써도 된다 ...
 function solution(n) {
-    return n % 2 === 0 ? "수박".repeat(n / 2) : "수박".repeat(n).slice(0, n);
+    return "수박".repeat(n).slice(0, n);
 }
