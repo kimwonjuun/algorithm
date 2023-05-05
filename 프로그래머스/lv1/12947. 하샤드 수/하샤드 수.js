628a4d9f4c1be352714709d06b5db4b0507a92a9
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-05-06 02:19:17 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,4 +1,4 @@
 function solution(x) {
-    const sum = x.toString().split("").map(Number).reduce((a, b) => a + b, 0);
+    const sum = x.toString().split("").map(Number).reduce((acc, cur) => acc + cur, 0);
     return x % sum === 0 ? true : false;
 }
