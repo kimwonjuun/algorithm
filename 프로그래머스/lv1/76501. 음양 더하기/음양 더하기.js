e9ab4eba33fab7d501d6f87231b8339d33a044dc
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-05-14 17:08:22 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,10 @@
 function solution(absolutes, signs) {
-    const arr = [];
-    for (let i = 0; i < absolutes.length; i++) {
-        signs[i] ? arr.push(absolutes[i]) : arr.push(-absolutes[i]);
-    }
-    return arr.reduce((acc, cur) => acc + cur, 0);
+    // const arr = [];
+    // for (let i = 0; i < absolutes.length; i++) {
+    //     signs[i] ? arr.push(absolutes[i]) : arr.push(-absolutes[i]);
+    // }
+    // return arr.reduce((acc, cur) => acc + cur, 0);
+    
+    // reduce 안에 넣어서 한 줄에 끝낼 수 있음
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
