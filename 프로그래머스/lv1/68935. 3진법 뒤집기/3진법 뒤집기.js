77f49e99d9f4fc756daa7ff2973856ae7da43b7e
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-05-18 20:31:07 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,6 +1,8 @@
 function solution(n) {
-    const tenary = n.toString(3);
-    const reversed_tenary = tenary.split("").reverse().join("");
-    const base = parseInt(reversed_tenary, 3);
-    return base;
+    // const tenary = n.toString(3);
+    // const reversed_tenary = tenary.split("").reverse().join("");
+    // const base = parseInt(reversed_tenary, 3);
+    // return base;
+    
+    return parseInt([...n.toString(3)].reverse().join(""), 3);
 }
