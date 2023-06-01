50e40cc711d8e5e5cae72e322190f30a9d838ae4
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-01 18:24:39 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,18 +1,18 @@
-// function solution(myString) {
-//     return myString
-//         .split("")
-//         .map(v => v === "a" ? v.toUpperCase() : v === v.toUpperCase() ? v.toLowerCase() : v)
-//         .join("");
-// }
-
 function solution(myString) {
-    return myString.split("").map((v) => {
-        if (v === "a") {
-            return v.toUpperCase();
-        } else if (v !== "A" && v === v.toUpperCase()) {
-            return v.toLowerCase();
-        } else {
-            return v;
-        }
-    }).join('');
+    return myString
+        .split("")
+        .map(v => v === "a" ? v.toUpperCase() : v !== "A" && v === v.toUpperCase() ? v.toLowerCase() : v)
+        .join("");
 }
+
+// function solution(myString) {
+//     return myString.split("").map((v) => {
+//         if (v === "a") {
+//             return v.toUpperCase();
+//         } else if (v !== "A" && v === v.toUpperCase()) {
+//             return v.toLowerCase();
+//         } else {
+//             return v;
+//         }
+//     }).join('');
+// }
