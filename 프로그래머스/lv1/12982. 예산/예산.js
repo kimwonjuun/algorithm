e24715b513cfc3a9989332dc836052576ebb8370
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-07 10:14:30 +0900

    [level 1] Title: 예산, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,17 +1,7 @@
 function solution(d, budget) {
-    
-    let result = 0;
-    const arr = d.sort((a, b) => a - b);
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (budget >= arr[i]) {
-            budget -= arr[i];
-            result++;
-        } else {
-            break;
-        }
-    }
-    
-    return result;
-}
+    d.sort((a, b) => a - b);
 
+    while (d.reduce((a, b) => (a + b), 0) > budget) d.pop();
+
+    return d.length;
+}
