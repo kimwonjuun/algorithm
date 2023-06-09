4e8d84b0b014c50bb3ee8ecb25b676f2eda82339
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-09 11:27:00 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,7 +1,26 @@
- function solution(a, b, c) {
-     return a != b && a != c && b != c
-     ? a + b + c
-     : a == b && a == c && b == c
-     ? (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
-     : (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
- }
+ // function solution(a, b, c) {
+ //     return a != b && a != c && b != c
+ //     ? a + b + c
+ //     : a == b && a == c && b == c
+ //     ? (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+ //     : (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+ // }
+
+const solution = (a, b, c) => {
+    const set = new Set([a, b, c]);
+    switch ([...set].length) {
+        case 1: return calculate([a, b, c], 3);
+        case 2: return calculate([a, b, c], 2);
+        case 3: return calculate([a, b, c]);
+    }
+};
+
+const calculate = (inc, n=1) => {
+    const [a, b, c] = inc;
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i)
+    }
+
+    return result;
+};
