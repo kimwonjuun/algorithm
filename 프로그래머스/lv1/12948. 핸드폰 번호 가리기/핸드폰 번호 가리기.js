e8bc1cb4d641ee47a339ced5f994e5d0ce1ac194
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-18 13:25:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,5 @@
-function solution(phone_number) {
-    return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
-}
+// function solution(phone_number) {
+//     return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+// }
+
+const solution = n => [...n].fill("*",0,n.length-4).join("")
