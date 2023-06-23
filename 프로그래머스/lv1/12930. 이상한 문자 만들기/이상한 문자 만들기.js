04e8d6898ccfeef076f9a64bc266d4973cec98c1
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-23 17:01:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,9 +1,14 @@
-function solution(s) {
-    return s.split(" ").map((word) => {
-        let result = "";
-        for (let i = 0; i < word.length; i++){
-            i % 2 === 0 ? result += word[i].toUpperCase() : result += word[i].toLowerCase();
-        }
-        return result;
-    }).join(" ");
+// function solution(s) {
+//     return s.split(" ").map((word) => {
+//         let result = "";
+//         for (let i = 0; i < word.length; i++){
+//             i % 2 === 0 ? result += word[i].toUpperCase() : result += word[i].toLowerCase();
+//         }
+//         return result;
+//     }).join(" ");
+// }
+
+function solution(s){
+  return s.toUpperCase().replace(/(\w)(\w)/g, function(a){return a[0].toUpperCase()+a[1].toLowerCase();})
+
 }
