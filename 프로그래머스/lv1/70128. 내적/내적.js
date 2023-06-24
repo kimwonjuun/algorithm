66217a21f95c95fd89492db30df9f09de910be5c
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-24 19:02:06 +0900

    [level 1] Title: 내적, Time: 0.36 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,12 +1,12 @@
 function solution(a, b) {
-    // const result = [];
-    // for (let i = 0; i < a.length; i++) {
-    // result.push(a[i] * b[i]);
-    // }
-    // return result.reduce((acc, cur) => acc + cur, 0);
+    const result = [];
+    for (let i = 0; i < a.length; i++) {
+    result.push(a[i] * b[i]);
+    }
+    return result.reduce((acc, cur) => acc + cur, 0);
     
     // return a.map((a, i) => a * b[i]).reduce((acc, cur) => acc + cur, 0);
     
     // 가장 빠름
-    return a.reduce((acc, cur, idx) => acc += cur * b[idx], 0);
+    // return a.reduce((acc, cur, idx) => acc += cur * b[idx], 0);
 }
