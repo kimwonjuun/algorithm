bab3d93914d40f93f0104537669d1ecc1ee2b093
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-25 17:24:30 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,11 @@
-// function solution(n) {
-//     let sum = 0;
-//     for (let i = 0; i <= n; i++) {
-//         if (n % i === 0) sum += i;
-//     }
-//     return sum;
-// }
-
-function solution(n, a=0, b=0) {
-    return n<=a/2?b:solution(n,a+1,b+=n%a?0:a);
+function solution(n) {
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) sum += i;
+    }
+    return sum;
 }
+
+// function solution(n, a=0, b=0) {
+//     return n<=a/2?b:solution(n,a+1,b+=n%a?0:a);
+// }
