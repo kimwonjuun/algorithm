2a6b728e58b3802814423dc04b6a14c2018b1b6b
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-28 23:24:45 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,7 +1,17 @@
-function solution(s) {
-    if(s.length != 4 && s.length != 6) return false;
-    for(var i = 0; i < s.length; i++) {
-        if(isNaN(Number(s[i]))) return false;
-    }
-    return true;
+// function solution(s) {
+//     const regex = /^[0-9]+$/;
+//     return (s.length === 4 || s.length === 6) && regex.test(s);
+// }
+
+// function solution(s) {
+//     if(s.length != 4 && s.length != 6) return false;
+//     for(var i = 0; i < s.length; i++) {
+//         if(isNaN(Number(s[i]))) return false;
+//     }
+//     return true;
+// }
+
+function solution(s){
+    const regex = /^\d{6}$|^\d{4}$/;
+    return regex.test(s);
 }
