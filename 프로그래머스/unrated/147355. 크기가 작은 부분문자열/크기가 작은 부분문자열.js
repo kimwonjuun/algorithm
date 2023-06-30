8720e3b0b96f9ba916f6a452dc3c3ce726a53d01
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-06-30 20:07:35 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,26 +1,26 @@
-// function solution(t, p) {
-//     const tNum = Number(t);
-//     const pNum = Number(p);
-    
-//     let count = 0;
-    
-//     for (let i = 0; i <= t.length - p.length; i++) {
-//         const substring = t.slice(i, i + p.length);
-//         const substringNum = Number(substring);
-        
-//         if (pNum >= substringNum) { 
-//             count++;
-//         }
-//     }
-    
-//     return count;
-// }
-
 function solution(t, p) {
+    const tNum = Number(t);
+    const pNum = Number(p);
+    
     let count = 0;
-    for(let i=0; i<=t.length-p.length; i++) {
-        let value = t.slice(i, i+p.length);
-        if(+p >= +value) count++;
+    
+    for (let i = 0; i <= t.length - p.length; i++) {
+        const substring = t.slice(i, i + p.length);
+        const substringNum = Number(substring);
+        
+        if (pNum >= substringNum) { 
+            count++;
+        }
     }
+    
     return count;
 }
+
+// function solution(t, p) {
+//     let count = 0;
+//     for(let i=0; i<=t.length-p.length; i++) {
+//         let value = t.slice(i, i+p.length);
+//         if(+p >= +value) count++;
+//     }
+//     return count;
+// }
