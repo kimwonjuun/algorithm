bbe41225ca58784eb39540b990fb42c0a78db9a2
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-01 21:30:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,6 @@
 function solution(s) {
-    const num_arr = s.split(" ").map(Number);
-    const max_num = Math.max(...num_arr);
-    const min_num = Math.min(...num_arr);
-    return min_num + " " + max_num;
-
+    const numArr = s.split(" ").map(Number);
+    const maxNum = Math.max(...numArr);
+    const minNum = Math.min(...numArr);
+    return minNum + " " + maxNum;
 }
