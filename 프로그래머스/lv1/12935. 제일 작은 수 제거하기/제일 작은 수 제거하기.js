04e6b7425b1e8d9d205f99e6e2e37a8e53f62e90
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-08 17:46:29 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,10 +1,10 @@
 function solution(arr) {
-    // 가장 작은 수 구하기 Math.min()
+    // 가장 작은 수 구하기. Math.min()
     let min_num = Math.min(...arr);
-    // 가장 작은 수의 인덱스 구하기
+    // 가장 작은 수의 인덱스 구하기.
     let index = arr.indexOf(min_num);
-    // 배열의 가장 작은 수 제거
+    // 배열의 가장 작은 수 제거.
     arr.splice(index, 1);
-    // 배열의 길이가 1 이상이면 배열 출력 아니면 [-1] 출력
+    // 배열의 길이가 1 이상이면 배열 출력 아니면 [-1] 출력.
     return arr.length > 1 ? arr : [-1];
 }
